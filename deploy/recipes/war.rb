ef9a72287831a5d9fea5f95c7e2139a141231530
Alexander De Leon <me@alexdeleon.name>
2013-06-10 19:21:50 +0200

    Added debug log

--- deploy/recipes/war.rb
+++ deploy/recipes/war.rb
@@ -1,4 +1,6 @@
 node[:deploy].each do |application, deploy|
+	Chef::Log.debug("Deploying WAR application #{application} with type #{deploy[application]}")
+
 	remote_file "#{deploy[application][:deploy_to]}/#{application}.war"
 	remote_file deploy[application][:url]
 	owner "tomcat"

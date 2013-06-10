d7b505b2c447d11a69a0d9ad9f8625589b7b04ee
Alexander De Leon <me@alexdeleon.name>
2013-06-10 19:20:10 +0200

    reimplemented war deploy recipe using remote_file

--- deploy/recipes/war.rb
+++ deploy/recipes/war.rb
@@ -1,15 +1,7 @@
-include_recipe 'deploy'
-
 node[:deploy].each do |application, deploy|
-
-  opsworks_deploy_dir do
-    user 'tomcat'
-    group 'tomcat'
-    path '/var/lib/tomcat7/webapps'
-  end
-
-  opsworks_deploy do
-    app application
-    deploy_data deploy
-  end
+	remote_file "#{deploy[application][:deploy_to]}/#{application}.war"
+	remote_file deploy[application][:url]
+	owner "tomcat"
+	group "tomcat"
+	action :create
 end

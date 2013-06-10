b6415a2e581de767fdf0e4012cb70ac723c2718d
Alexander De Leon <me@alexdeleon.name>
2013-06-10 19:36:52 +0200

    Fix recipe deploy::war

--- deploy/recipes/war.rb
+++ deploy/recipes/war.rb
@@ -1,9 +1,10 @@
 node[:deploy].each do |application, deploy|
-	Chef::Log.debug("Deploying WAR application #{application} with type #{node[application]}")
+	Chef::Log.debug("Deploying WAR application #{application} from #{node[application][:url]}")
 
-	remote_file "#{node[application][:deploy_to]}/#{application}.war"
-	remote_file node[application][:url]
-	owner "tomcat"
-	group "tomcat"
-	action :create
+	remote_file "#{node[application][:deploy_to]}/#{application}.war" do
+		source node[application][:url]
+		owner "tomcat"
+		group "tomcat"
+		action :create
+	end
 end
